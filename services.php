ff957fcb7b9be42873172a553d92e683425e4e05
bohdan <yatsunbohdan194@gmail.com>
2024-01-15 15:38:20 +0200

    add wrap for page services

--- services.php
+++ services.php
@@ -4,18 +4,20 @@ TempLate name: Services
 */
 get_header();
 ?>
-<?php
-  get_template_part('templates/services/hero-section');
-  get_template_part('templates/services/affect-section');
-  get_template_part('templates/services/quality-section');
-  get_template_part('templates/services/pr-section');
-  get_template_part('templates/services/process-section');
-?>
-<?php 
-  get_template_part('templates/main/packages-section');
-  get_template_part('templates/main/reviews-section');
-  get_template_part('templates/main/specialist-section');
-  get_template_part('templates/faq');
-?>
+<main class="services">
+  <?php
+    get_template_part('templates/services/hero-section');
+    get_template_part('templates/services/affect-section');
+    get_template_part('templates/services/quality-section');
+    get_template_part('templates/services/pr-section');
+    get_template_part('templates/services/process-section');
+  ?>
+  <?php 
+    get_template_part('templates/main/packages-section');
+    get_template_part('templates/main/reviews-section');
+    get_template_part('templates/main/specialist-section');
+    get_template_part('templates/faq');
+  ?>
   <?php get_template_part( 'pages/button-top');?>
+</main>
 <?php get_footer(); ?>
