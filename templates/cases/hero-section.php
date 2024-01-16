21f1596e28e688e13fd7c147f1879ff9090102ca
bohdan <yatsunbohdan194@gmail.com>
2024-01-16 19:14:21 +0200

    add first block page cases

---
+++ templates/cases/hero-section.php
@@ -0,0 +1,37 @@
+<seciton class="cases__hero_cases">
+  <div class="hero_cases__container container">
+    <div class="hero_cases__wrap">
+      <div class="hero_cases__content">
+        <h1 class="hero_cases__title section_title"><span class="span_title_violet">Game</span>development niche: Growing organic traffic X15</h1>
+        <a href="#" class="hero_cases__read link_read_more">read more</a>
+        <div class="hero_cases__user">
+          <div class="user__wrap">
+            <span class="user__date text_grey">23 September, 2023</span>
+            <span class="user__view text_grey">447 views</span>
+          </div>
+          <div class="user__wrap">
+            <div class="user__avatar">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/reviews/2.jpg" alt="img-avatar">
+            </div>
+            <span class="user__name">Maryna Ruban</span>
+          </div>
+        </div>
+      </div>
+      <div class="hero_cases__articles">
+        <h2 class="articles__title section_title">Latest articles</h2>
+        <div class="articles__block">
+          <h3 class="articles__title_articles section_title">game development niche: Growing organic traffic X15</h3>
+          <span class="articles__date text_grey">23 September, 2023</span>
+        </div>
+        <div class="articles__block">
+          <h3 class="articles__title_articles section_title">game development niche: Growing organic traffic X15</h3>
+          <span class="articles__date text_grey">23 September, 2023</span>
+        </div>
+        <div class="articles__block">
+          <h3 class="articles__title_articles section_title">game development niche: Growing organic traffic X15</h3>
+          <span class="articles__date text_grey">23 September, 2023</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</seciton>
