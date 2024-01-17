8b14e229c3197b37a18835bead07c9ad58e91f59
bohdan <yatsunbohdan194@gmail.com>
2024-01-17 18:19:11 +0200

    add section posts cases

---
+++ templates/cases/posts-section.php
@@ -0,0 +1,121 @@
+<div class="cases_posts">
+  <div class="cases_posts__container container">
+    <div class="cases_posts__cases_post">
+      <div class="cases_post__img">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/cases/1.png" alt="img-post">
+      </div>
+      <h3 class="cases_post__title section_title">Lorem Ipsum is simply dummy text of the printing</h3>
+      <p class="cases_post__text text_grey">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled</p>
+      <a href="#" class="cases_post__link link_read_more">READ MORE</a>
+      <div class="cases_post__wrap">
+        <span class="cases_post__date text_grey">23 September, 2023</span>
+        <span class="cases_post__view text_grey">447 views</span>
+      </div>
+      <span class="cases_post__name text_grey">Dmytro Kovshun</span>
+    </div>
+    <div class="cases_posts__cases_post">
+      <div class="cases_post__img">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/cases/2.png" alt="img-post">
+      </div>
+      <h3 class="cases_post__title section_title">Lorem Ipsum is simply dummy text of the printing</h3>
+      <p class="cases_post__text text_grey">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled</p>
+      <a href="#" class="cases_post__link link_read_more">READ MORE</a>
+      <div class="cases_post__wrap">
+        <span class="cases_post__date text_grey">23 September, 2023</span>
+        <span class="cases_post__view text_grey">447 views</span>
+      </div>
+      <span class="cases_post__name text_grey">Dmytro Kovshun</span>
+    </div>
+    <div class="cases_posts__cases_post">
+      <div class="cases_post__img">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/cases/3.png" alt="img-post">
+      </div>
+      <h3 class="cases_post__title section_title">Lorem Ipsum is simply dummy text of the printing</h3>
+      <p class="cases_post__text text_grey">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled</p>
+      <a href="#" class="cases_post__link link_read_more">READ MORE</a>
+      <div class="cases_post__wrap">
+        <span class="cases_post__date text_grey">23 September, 2023</span>
+        <span class="cases_post__view text_grey">447 views</span>
+      </div>
+      <span class="cases_post__name text_grey">Dmytro Kovshun</span>
+    </div>
+    <div class="cases_posts__cases_post">
+      <div class="cases_post__img">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/cases/4.png" alt="img-post">
+      </div>
+      <h3 class="cases_post__title section_title">Lorem Ipsum is simply dummy text of the printing</h3>
+      <p class="cases_post__text text_grey">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled</p>
+      <a href="#" class="cases_post__link link_read_more">READ MORE</a>
+      <div class="cases_post__wrap">
+        <span class="cases_post__date text_grey">23 September, 2023</span>
+        <span class="cases_post__view text_grey">447 views</span>
+      </div>
+      <span class="cases_post__name text_grey">Dmytro Kovshun</span>
+    </div>
+    <div class="cases_posts__cases_post">
+      <div class="cases_post__img">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/cases/5.png" alt="img-post">
+      </div>
+      <h3 class="cases_post__title section_title">Lorem Ipsum is simply dummy text of the printing</h3>
+      <p class="cases_post__text text_grey">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled</p>
+      <a href="#" class="cases_post__link link_read_more">READ MORE</a>
+      <div class="cases_post__wrap">
+        <span class="cases_post__date text_grey">23 September, 2023</span>
+        <span class="cases_post__view text_grey">447 views</span>
+      </div>
+      <span class="cases_post__name text_grey">Dmytro Kovshun</span>
+    </div>
+    <div class="cases_posts__cases_post">
+      <div class="cases_post__img">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/cases/6.png" alt="img-post">
+      </div>
+      <h3 class="cases_post__title section_title">Lorem Ipsum is simply dummy text of the printing</h3>
+      <p class="cases_post__text text_grey">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled</p>
+      <a href="#" class="cases_post__link link_read_more">READ MORE</a>
+      <div class="cases_post__wrap">
+        <span class="cases_post__date text_grey">23 September, 2023</span>
+        <span class="cases_post__view text_grey">447 views</span>
+      </div>
+      <span class="cases_post__name text_grey">Dmytro Kovshun</span>
+    </div>
+    <div class="cases_posts__cases_post">
+      <div class="cases_post__img">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/cases/7.png" alt="img-post">
+      </div>
+      <h3 class="cases_post__title section_title">Lorem Ipsum is simply dummy text of the printing</h3>
+      <p class="cases_post__text text_grey">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled</p>
+      <a href="#" class="cases_post__link link_read_more">READ MORE</a>
+      <div class="cases_post__wrap">
+        <span class="cases_post__date text_grey">23 September, 2023</span>
+        <span class="cases_post__view text_grey">447 views</span>
+      </div>
+      <span class="cases_post__name text_grey">Dmytro Kovshun</span>
+    </div>
+    <div class="cases_posts__cases_post">
+      <div class="cases_post__img">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/cases/8.png" alt="img-post">
+      </div>
+      <h3 class="cases_post__title section_title">Lorem Ipsum is simply dummy text of the printing</h3>
+      <p class="cases_post__text text_grey">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled</p>
+      <a href="#" class="cases_post__link link_read_more">READ MORE</a>
+      <div class="cases_post__wrap">
+        <span class="cases_post__date text_grey">23 September, 2023</span>
+        <span class="cases_post__view text_grey">447 views</span>
+      </div>
+      <span class="cases_post__name text_grey">Dmytro Kovshun</span>
+    </div>
+    <div class="cases_posts__cases_post">
+      <div class="cases_post__img">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/cases/9.png" alt="img-post">
+      </div>
+      <h3 class="cases_post__title section_title">Lorem Ipsum is simply dummy text of the printing</h3>
+      <p class="cases_post__text text_grey">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled</p>
+      <a href="#" class="cases_post__link link_read_more">READ MORE</a>
+      <div class="cases_post__wrap">
+        <span class="cases_post__date text_grey">23 September, 2023</span>
+        <span class="cases_post__view text_grey">447 views</span>
+      </div>
+      <span class="cases_post__name text_grey">Dmytro Kovshun</span>
+    </div>
+  </div>
+</div>
