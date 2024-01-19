69f49b96327249c37739fe5820ebf48d4a7cd7a9
bohdan <yatsunbohdan194@gmail.com>
2024-01-19 11:23:07 +0200

    add section kevuru for page cases-result

---
+++ templates/cases-result/kevuru-section.php
@@ -0,0 +1,17 @@
+<section class="cases_result__kevuru">
+  <div class="kevuru__container container">
+    <div class="kevuru__img">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/cases-result-kevuru/kevuru.png" alt="img-kevuru">
+    </div>
+    <div class="kevuru__content_kevuru">
+      <h3 class="content_kevuru__title section_title">Kevuru.Games.</h3>
+      <div  class="content_kevuru__text text_grey">
+        <p>In this case, we describe the history of successful work with one of the most famous Game Development companies - Kevuru.Games.</p>
+        <br>
+        <p>The company approached us in 2020 with almost zero traffic and no backlinks.</p>
+        <br>
+        <p><span>Over the past 3 years,</span> thanks to the high-quality and steady growth of referral mass, we have increased traffic to 27,513 visits per month and made more than 900 organic outreach placements.</p>
+      </div>
+    </div>
+  </div>
+</section>
