66a186c37af5609ea3e8eef6ebeb6cce868d96da
bohdan <yatsunbohdan194@gmail.com>
2024-01-19 11:22:36 +0200

    add page hero for cases-result

---
+++ templates/cases-result/hero-section.php
@@ -0,0 +1,19 @@
+<seciton class="cases_result__hero_cases_result">
+  <div class="hero_cases_result__container container">
+    <div class="hero_cases_result__wrap">
+      <div class="hero_cases_result__content">
+        <h2 class="hero_cases_result__title section_title"><span class="span_title_violet">Game</span>development niche: Growing organic traffic X15</h2>
+      </div>
+      <div class="hero_cases_result__results">
+        <div class="result__block">
+         <span class="result__number section_title">27513+</span>
+         <span class="result__text">visits per month</span>
+        </div>
+        <div class="result__block">
+          <span class="result__number section_title">900+</span>
+          <span class="result__text">organic outreach placements</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</seciton>
