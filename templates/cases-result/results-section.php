e3fec56c5df126d6966224db570310c5143927a5
bohdan <yatsunbohdan194@gmail.com>
2024-01-19 11:24:22 +0200

    add section result for page cases-result

---
+++ templates/cases-result/results-section.php
@@ -0,0 +1,44 @@
+<section class="cases_result__result">
+  <div class="container">
+    <div class="result__placements">
+      <div class="placements__wrap">
+        <h2 class="placements__title section_title"><span class="span_title_violet">results</span></h2>
+        <span class="placements__span section_title">Examples of placements:</span>
+        <div class="block_result__list_placements">
+          <ul>
+            <li><a class="text_grey" href="https://pollthepeople.app/a-comprehensive-exploration-of-ai-driven-user-experience-design-in-games/">https://pollthepeople.app/a-comprehensive-exploration-of-ai-driven-user-experience-design-in-games/</a></li>
+            <li><a class="text_grey" href="https://blog.powr.io/e-commerce-evolution-3d-game-animation-for-consumer-interaction">https://blog.powr.io/e-commerce-evolution-3d-game-animation-for-consumer-interaction</a></li>
+            <li><a class="text_grey" href="https://www.jootoor.com/illustration-impact/">https://www.jootoor.com/illustration-impact/</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="placements__wrap">
+        <div class="placements__img">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/cases-result-results/1.jpg" alt="img-graphic">
+        </div>
+        <span class="placements__info result_info_text">Growth of Reffering Domains from 114 to 1951</span>
+      </div>
+    </div>
+      
+    <div class="result__graphics_result">
+      <div class="graphics_result__wrap">
+        <div class="graphics_result__img">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/cases-result-results/2.jpg" alt="img-graphic">
+        </div>
+        <div class="graphics_result__img">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/cases-result-results/3.jpg" alt="img-graphic">
+        </div>
+        <span class="graphics_result__info result_info_text">Traffic growth from 1842 to 26780 visits per month</span>
+      </div>
+      <div class="graphics_result__wrap">
+        <div class="graphics_result__img">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/cases-result-results/4.jpg" alt="img-graphic">
+        </div>
+        <div class="graphics_result__img">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/cases-result-results/5.jpg" alt="img-graphic">
+        </div>
+        <span class="graphics_result__info padding result_info_text">DR (Domain Rating) growth from 23 to 52</span>
+      </div>
+    </div>
+  </div>
+</section>
