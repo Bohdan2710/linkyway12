e99bfa3259c75e56efec8e3bb14a09ff03c8ba3a
bohdan <yatsunbohdan194@gmail.com>
2024-01-25 14:59:21 +0200

    fix

--- templates/main/reviews-section.php
+++ templates/main/reviews-section.php
@@ -12,8 +12,10 @@
               <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/icon/star-fiverr-footer.svg" alt="img-stars">
             </div>
             <p class="block_reviews__text text_grey">
-              The Linkyway team built trust and quality backlinks for my website from scratch. 
-              Particularly impressive was their consistent communication and generation of innovative ideas to promote my company. Although we have only recently started working together, we are already seeing significant results.
+            The Linkyway team built trust and quality backlinks for my website from scratch. 
+            <br>
+            <br>
+            Particularly impressive was their consistent communication and generation of innovative ideas to promote my company. Although we have only recently started working together, we are already seeing significant results.
             </p>
             <div class="block_reviews__user_reviews">
               <div class="user_reviews__avatar">
@@ -40,9 +42,9 @@
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/reviews/2.jpg" alt="img-avatar">
               </div>
               <div class="user_reviews__wrap">
-                <span class="user_reviews__name text_grey">Maryna Ruban</span>
-                <span class="user_reviews__position">COO, Luxeo</span>
-                <span class="user_reviews__linkedIn">Linkedin: <a href="#">maryna-ruban</a></span>
+                <span class="user_reviews__name text_grey">Alex Omelyanenko</span>
+                <span class="user_reviews__position">СМО, omelyanenko.pro</span>
+                <span class="user_reviews__linkedIn">Linkedin: <a href="#">Alexander Omelyanenko</a></span>
               </div>
             </div>
           </div>
@@ -53,7 +55,7 @@
               <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/icon/star-fiverr-footer.svg" alt="img-stars">
             </div>
             <p class="block_reviews__text text_grey">
-              We have been working with the Linkyway team for a long time and have never once doubted their expertise. They conducted a detailed SEO analysis of our website and identified the main vectors of promotion. They select only the top sites and write high-quality texts. I definitely recommend Linkyway if you need complex link building.
+            We have been working with the Linkyway team for a long time and have never once doubted their expertise. They conducted a detailed SEO analysis of our website and identified the main vectors of promotion. They select only the top sites and write high-quality texts. I definitely recommend Linkyway if you need complex link building.
             </p>
             <div class="block_reviews__user_reviews">
               <div class="user_reviews__avatar">
@@ -73,16 +75,18 @@
               <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/icon/star-fiverr-footer.svg" alt="img-stars">
             </div>
             <p class="block_reviews__text text_grey">
-              We have been working with the Linkyway team for a long time and have never once doubted their expertise. They conducted a detailed SEO analysis of our website and identified the main vectors of promotion. They select only the top sites and write high-quality texts. I definitely recommend Linkyway if you need complex link building.
+            The Linkyway team built trust and quality backlinks for my website from scratch. 
+            <br>
+            Particularly impressive was their consistent communication and generation of innovative ideas to promote my company. Although we have only recently started working together, we are already seeing significant results.
             </p>
             <div class="block_reviews__user_reviews">
               <div class="user_reviews__avatar">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/reviews/3.jpg" alt="img-avatar">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/reviews/1.jpg" alt="img-avatar">
               </div>
               <div class="user_reviews__wrap">
-                <span class="user_reviews__name text_grey">Dmitry Vishnyov</span>
-                <span class="user_reviews__position">Co-founder & CBDO,  IT Outposts</span>
-                <span class="user_reviews__linkedIn">Linkedin: <a href="#">dmitriy-vishnyov</a></span>
+                <span class="user_reviews__name text_grey">Oleg Shumar</span>
+                <span class="user_reviews__position">CEO & Founder, GetTrusted.io</span>
+                <span class="user_reviews__linkedIn">Linkedin: <a href="#">oshumar</a></span>
               </div>
             </div>
           </div>
@@ -93,16 +97,16 @@
               <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/icon/star-fiverr-footer.svg" alt="img-stars">
             </div>
             <p class="block_reviews__text text_grey">
-              We have been working with the Linkyway team for a long time and have never once doubted their expertise. They conducted a detailed SEO analysis of our website and identified the main vectors of promotion. They select only the top sites and write high-quality texts. I definitely recommend Linkyway if you need complex link building.
+              We often order Linkbuilding for our clients from Linkyway. And we can say they are the best contractors we have worked with. Only they can get links that meet all the requirements and rates. All work is always completed by the deadline.
             </p>
             <div class="block_reviews__user_reviews">
               <div class="user_reviews__avatar">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/reviews/3.jpg" alt="img-avatar">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/reviews/2.jpg" alt="img-avatar">
               </div>
               <div class="user_reviews__wrap">
-                <span class="user_reviews__name text_grey">Dmitry Vishnyov</span>
-                <span class="user_reviews__position">Co-founder & CBDO,  IT Outposts</span>
-                <span class="user_reviews__linkedIn">Linkedin: <a href="#">dmitriy-vishnyov</a></span>
+                <span class="user_reviews__name text_grey">Alex Omelyanenko</span>
+                <span class="user_reviews__position">СМО, omelyanenko.pro</span>
+                <span class="user_reviews__linkedIn">Linkedin: <a href="#">Alexander Omelyanenko</a></span>
               </div>
             </div>
           </div>
@@ -113,27 +117,7 @@
               <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/icon/star-fiverr-footer.svg" alt="img-stars">
             </div>
             <p class="block_reviews__text text_grey">
-              We have been working with the Linkyway team for a long time and have never once doubted their expertise. They conducted a detailed SEO analysis of our website and identified the main vectors of promotion. They select only the top sites and write high-quality texts. I definitely recommend Linkyway if you need complex link building.
-            </p>
-            <div class="block_reviews__user_reviews">
-              <div class="user_reviews__avatar">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/reviews/3.jpg" alt="img-avatar">
-              </div>
-              <div class="user_reviews__wrap">
-                <span class="user_reviews__name text_grey">Dmitry Vishnyov</span>
-                <span class="user_reviews__position">Co-founder & CBDO,  IT Outposts</span>
-                <span class="user_reviews__linkedIn">Linkedin: <a href="#">dmitriy-vishnyov</a></span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="swiper-slide">
-          <div class="reviews__block_reviews">
-            <div class="block_reviews__stars">
-              <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/icon/star-fiverr-footer.svg" alt="img-stars">
-            </div>
-            <p class="block_reviews__text text_grey">
-              We have been working with the Linkyway team for a long time and have never once doubted their expertise. They conducted a detailed SEO analysis of our website and identified the main vectors of promotion. They select only the top sites and write high-quality texts. I definitely recommend Linkyway if you need complex link building.
+            We have been working with the Linkyway team for a long time and have never once doubted their expertise. They conducted a detailed SEO analysis of our website and identified the main vectors of promotion. They select only the top sites and write high-quality texts. I definitely recommend Linkyway if you need complex link building.
             </p>
             <div class="block_reviews__user_reviews">
               <div class="user_reviews__avatar">
